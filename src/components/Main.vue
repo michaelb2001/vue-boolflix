9e3f1a99be7ff6862bd0c9b9e20a0835b39dd6ab
michaelb2001 <brunco.michael@gmail.com>
2022-02-02 13:57:16 +0100

    header + main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="container-fluid">
-      <Header />
   </div>
 </template>
 
 <script>
-import Header from './componets/Header.vue';
 
 export default {
     name:"Main",
-    components:{
-        Header
-    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/vars.scss';
 
+.container-fluid{
+    min-height: 100vh;
+    background-color: $third-color;
+}
 </style>
