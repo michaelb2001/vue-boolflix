ead7b22291a20e0dc37a273e99e992be32dc239e
michaelb2001 <brunco.michael@gmail.com>
2022-02-02 14:03:50 +0100

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <Header />
-    <Main />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
-import Main from './components/Main.vue';
+
 export default {
   name: 'App',
   components: {
-    Header,
-    Main
+    Header
   }
 }
 </script>
