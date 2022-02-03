e4fa166d789f2f8fc6ac714344d16b72da651b71
michaelb2001 <brunco.michael@gmail.com>
2022-02-03 03:43:10 +0100

    cambio bandiera su infromazioni

--- src/components/commons/Cerca.vue
+++ src/components/commons/Cerca.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container d-flex flex-wrap align-items-center justify-content-end mx-2">
-      <input id="inputText" type="text" placeholder="write a title" class="rounded-pill px-2 py-0 mx-2"
-       v-model="ricercato">
-
-      <button class="btn btn-danger rounded-pill px-2 py-0" 
-      @click="$emit('cerca',ricercato)">Search</button>
+    <input id="inputText" type="text" placeholder="write a title" class="rounded-pill px-2 py-0 mx-2"
+    v-model="ricercato">
+    <button class="btn btn-danger rounded-pill px-2 py-0" 
+    @click="$emit('cerca',ricercato)">Search</button>
   </div>
 </template>
 
@@ -26,4 +25,9 @@ input{
     background-color: $second-color;
     color: $main-color;
 }
+
+img{
+    width: 100%;
+}
+
 </style>
